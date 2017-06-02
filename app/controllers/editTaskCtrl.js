cdfa5acf3b2c842a80da46c2e039d2c50790619b
Chaz Henricks <chazhenricks@gmail.com>
2017-06-02 12:15:59 -0500

    fixed edit button anchor

--- app/controllers/editTaskCtrl.js
+++ app/controllers/editTaskCtrl.js
@@ -5,7 +5,7 @@ app.controller('EditTaskCtrl', function($scope) {
   $scope.edittedTask = {};
 
   $scope.editTask = function() {
-    $scope.edittedTask.task =;
+    // $scope.edittedTask.task =;
   };
 
 });
